f880ab6a823ad422737d9f0ec95c97d9b66aed1c
nick <email@email.com>
2024-07-18 15:41:05 -0400

    removed debugging print statements

--- src/main.py
+++ src/main.py
@@ -2,9 +2,7 @@ from textnode import *
 from htmlnode import *
 
 def main():
-    textnode = TextNode("Some text", "bold", "www.me.com")
-    htmlnode = HTMLNode("p","this is a paragraph text","test",{"href": "https://www.google.com","target": "_blank"})
-    print(textnode)
-    text = htmlnode.props_to_html()
-    print(text)
+    pass
+
+
 main()
--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -14,18 +14,10 @@ delimiter_to_text_type = {
 
 def text_to_textnodes(text):
     node = split_nodes_delimiter([TextNode(text,'text')],'','text')
-    print(f'original_node: {node}')
-    
     node_list = split_nodes_image(node)
-    print(f'image_node: {node_list}')
-    
     node_list = split_nodes_link(node_list)
-    print(f'link node: {node_list}')
+
     for key in delimiter_to_text_type.keys():
             node_list = split_nodes_delimiter(node_list, key, 'text')
 
-    for node in node_list:
-        print(node)
     return node_list  
-
-test_case = text_to_textnodes('here is an ![image](image.com), and a [link](link.com).')
